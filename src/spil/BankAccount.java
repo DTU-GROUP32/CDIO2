e703f9588042f9f9ff796ed3668488a7afdc15d3
Viktor Poulsen <viktor@maigaard.io>
2016-10-15 08:54:08 +0200

    Minor Cleanup
    Cleared master branch. Ready for pull requests

--- src/spil/BankAccount.java
+++ src/spil/BankAccount.java
@@ -2,44 +2,9 @@ package spil;
 
 public class BankAccount {
 
-	private int balance;
-	//initiate Bank Account 
-	public BankAccount(int balance){
-		
-		if (balance < 0)
-			this.balance = 0;
-		else 
-			this.balance = balance;
+	public BankAccount(){
+
 	}
-	//se if player balance reached 3000
-//	public boolean isWin(){
-//		
-//		return this.balance >= 3000;
-//	}
-	
-	public void Deposit(int amount){
-		
-		
-	}
-	
-	public void Withdraw(int amount){
-		
-		if ((this.balance + amount)<0)
-			this.balance = 0;
-		else
-			this.balance = this.balance + amount;
-		
-		
-	}
-	public int getBalance() 
-	{
-		return this.balance;
-	}
-	
-	
-	
-	
-	
 
 
 }
