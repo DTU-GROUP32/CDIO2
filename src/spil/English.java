cce35b4b8596186a63700e91e6d2d89ef6936df3
Unknown <spirem@gmail.com>
2016-10-22 18:23:35 +0200

    Primarily implemented the language solution
    Also made some changes in Player and Bank to make them work, with the rest of the code and the class diagram.

---
+++ src/spil/English.java
@@ -0,0 +1,91 @@
+package spil;
+
+public class English implements Language {
+
+	English() {}
+	
+	@Override
+	public String welcomeMsg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String askForPlayerName(int playerNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String readyToBegin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String preMsg(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String rollResult(DiceCup diceCup) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String fieldMsg(DiceCup diceCup) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String postMsg(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String winnerMsg(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String menu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printRules() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printScore(Player[] players) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String changeDices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printDiceChangeSucces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printDiceChangeNotExecuted() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
