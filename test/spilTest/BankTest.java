3f1b068c47ba3786f125155aced542bc3208e0e8
Viktor Poulsen <viktor@maigaard.io>
2016-10-25 00:05:12 +0200

    Written unittest for Bank

--- test/spilTest/BankTest.java
+++ test/spilTest/BankTest.java
@@ -3,28 +3,35 @@ package spilTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import spil.Bank;
 
 import static org.junit.Assert.*;
 
 public class BankTest {
+    private Bank bank;
     @Before
     public void setUp() throws Exception {
-
+        bank = new Bank();
     }
 
     @After
     public void tearDown() throws Exception {
-
+        bank = null;
     }
 
     @Test
     public void changeBalance() throws Exception {
-
+        bank.changeBalance(-100);
+        assertEquals(bank.getBalance(), 900);
+        bank.changeBalance(100);
+        assertEquals(bank.getBalance(), 1000);
+        bank.changeBalance(-1001);
+        assertEquals(bank.getBalance(), 0);
     }
 
     @Test
     public void getBalance() throws Exception {
-
+        assertEquals(bank.getBalance(), 1000);
     }
 
 }
