b0bcf536473114671035b17e8bfc09b245d7eda5
Mads118 <vpmads@gmail.com>
2016-10-27 01:10:47 +0200

    made junittests

--- test/spilTest/languageTest/DanskTest.java
+++ test/spilTest/languageTest/DanskTest.java
@@ -4,95 +4,120 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import spil.Player;
+import spil.language.Dansk;
+import spil.DiceCup;
 import static org.junit.Assert.*;
 
 /**
  * Created by Viktor on 24/10/2016.
  */
 public class DanskTest {
-    @Before
-    public void setUp() throws Exception {
+	Dansk language; 
+	Player player; 
+	Player[] players;
+	DiceCup diceCup;
 
-    }
+	@Before
+	public void setUp() throws Exception {
+		language = new Dansk();
+		player = new Player();
+		diceCup = new DiceCup();
+		players = new Player[2];
+	}
 
-    @After
-    public void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
+		language = null;
+		player = null;
+		players = null;
+		diceCup = null;
+	}
 
-    }
+	@Test
+	public void welcomeMsg() throws Exception {
+		assertTrue(language.welcomeMsg() instanceof String);
 
-    @Test
-    public void welcomeMsg() throws Exception {
+	}
 
-    }
 
-    @Test
-    public void askForPlayerName() throws Exception {
+	@Test
+	public void askForPlayerName() throws Exception {
+		assertTrue(language.askForPlayerName(0) instanceof String);
+	}
 
-    }
+	@Test
+	public void readyToBegin() throws Exception {
+		assertTrue(language.readyToBegin() instanceof String);
+	}
 
-    @Test
-    public void readyToBegin() throws Exception {
+	@Test
+	public void preMsg() throws Exception {
+		assertTrue(language.preMsg(player) instanceof String);
+	}
 
-    }
+	@Test
+	public void rollResult() throws Exception {
+		assertTrue(language.rollResult(diceCup) instanceof String);
+	}
 
-    @Test
-    public void preMsg() throws Exception {
+	@Test
+	public void fieldMsg() throws Exception {
+		assertTrue(language.fieldMsg(diceCup) instanceof String);
+	}
 
-    }
 
-    @Test
-    public void rollResult() throws Exception {
+	@Test
+	public void postMsg() throws Exception {
+		assertTrue(language.postMsg(player) instanceof String);
+	}
 
-    }
 
-    @Test
-    public void fieldMsg() throws Exception {
+	@Test
+	public void winnerMsg() throws Exception {
+		assertTrue(language.winnerMsg(player) instanceof String);
+	}
 
-    }
 
-    @Test
-    public void postMsg() throws Exception {
+	@Test
+	public void menu() throws Exception {
+		assertTrue(language.menu() instanceof String);
+	}
 
-    }
 
-    @Test
-    public void winnerMsg() throws Exception {
+	@Test
+	public void printRules() throws Exception {
+		assertTrue(language.printRules() instanceof String);
+	}
 
-    }
 
-    @Test
-    public void menu() throws Exception {
+	@Test
+	public void printScore() throws Exception {
+		assertTrue(language.printScore(players) instanceof String); // Failure
+	}
 
-    }
 
-    @Test
-    public void printRules() throws Exception {
+	@Test
+	public void changeDices() throws Exception {
+		assertTrue(language.changeDices() instanceof String);
+	}
 
-    }
 
-    @Test
-    public void printScore() throws Exception {
+	@Test
+	public void printDiceChangeSucces() throws Exception {
+		assertTrue(language.printDiceChangeSucces() instanceof String);
 
-    }
+	}
 
-    @Test
-    public void changeDices() throws Exception {
+	@Test
+	public void printDiceChangeNotExecuted() throws Exception {
+		assertTrue(language.printDiceChangeNotExecuted() instanceof String);
+	}
 
-    }
 
-    @Test
-    public void printDiceChangeSucces() throws Exception {
-
-    }
-
-    @Test
-    public void printDiceChangeNotExecuted() throws Exception {
-
-    }
-
-    @Test
-    public void printGameMenu() throws Exception {
-
-    }
+	@Test
+	public void printGameMenu() throws Exception {
+		assertTrue(language.printGameMenu() instanceof String);
+	}
 
 }
